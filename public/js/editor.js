bccb5005e5b40fed9dfce496d2da7338c908a313
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2024-01-20 22:50:54 +0100

    h

--- public/js/editor.js
+++ public/js/editor.js
@@ -23,7 +23,7 @@ const uploadImage = (uploadFile, uploadType) => {
         const formdata = new FormData();
         formdata.append('image', file);
 
-        fetch('/public/uploads', {
+        fetch('/uploads', {
             method: 'post',
             body: formdata
         }).then(res => res.json())
